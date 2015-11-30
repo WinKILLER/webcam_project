33d6b4be796c4b3eee284e1fc947d20847363d0c
carlesOA <carles.oro@uvic.cat>
2015-11-30 17:49:53 +0100

    just a program to control a motor using arduino and ROS

--- pwm_arduino/pwm_arduino.ino
+++ pwm_arduino/pwm_arduino.ino
@@ -1,27 +1,39 @@
-int motorPin = 3;
-int velocidad = 100;
-void setup() 
-{ 
-  pinMode(motorPin, OUTPUT);
-  Serial.begin(9600);
-  //while (! Serial);
-  //Serial.println("Speed 0 to 255");
+#include "Arduino.h"
+
+#include <Servo.h> 
+#include <ros.h>
+#include <std_msgs/Int8.h>
+
+ros::NodeHandle  nh;
+int y,x;
+Servo servo;
+
+void servo_cb( const std_msgs::Int8& cmd_msg)
+{  
+  x=cmd_msg.data;
+  y=(((-9/7)*x)+90);
   
-} 
- 
- 
-void loop() 
-{ 
-  /*if (Serial.available())
-  {
-    int speed = Serial.parseInt();
-    if (speed >= 0 && speed <= 255)
-    {
-      analogWrite(motorPin, speed);
-    }
-  }*/
+  if (y < 0) y=0;
+   
+  if (y > 180) y=180;
   
-  analogWrite(motorPin, velocidad);
+  servo.write(y); //set servo angle, should be from 0-180  
+  digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+}
+
+
+ros::Subscriber<std_msgs::Int8> sub("servo", servo_cb);
+
+void setup(){
+  pinMode(13, OUTPUT);
+
+  nh.initNode();
+  nh.subscribe(sub);
   
-  
-} 
+  servo.attach(9); //attach it to pin 9
+}
+
+void loop(){
+  nh.spinOnce();
+  delay(1);
+}
