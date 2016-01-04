c37c96341b51544ea9968cb8f27bf26b842e0edb
oriolorra <oriolorraserra@gmail.com>
2016-01-04 23:33:20 +0100

    launch face_detector deleted

--- ros_img_processor/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oos/catkin_ws/src/webcam_project/ros_img_processor
-# Build directory: /home/oos/catkin_ws/src/webcam_project/ros_img_processor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
