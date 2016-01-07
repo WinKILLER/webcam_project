665b03e317f9339eb3bb86b036562d69476eb78e
Juampe López <jplopez@circutor.com>
2016-01-07 16:29:01 +0100

    We can detect faces now!

--- ros_face_detector/src/ros_face_detector_node.cpp
+++ ros_face_detector/src/ros_face_detector_node.cpp
@@ -1,24 +1,27 @@
 #include "ros_face_detector_node.h"
+#include <ros/console.h>
 
 RosFaceDetectorNode::RosFaceDetectorNode():
     nh_(ros::this_node::getName()),
     img_tp_(nh_)
 {
-    //loop rate [hz], Could be set from a yaml file 
-    rate_=10; 
-    
+    //loop rate [hz], Could be set from a yaml file
+    rate_=10;
+
     //sets publishers
+    detect_msg_.layout.dim.resize(1);
     detect_msg_.layout.dim[0].label = "states_detected";
     detect_msg_.layout.dim[0].size = 4;
+    detect_msg_.data.resize(4);
 
     detector_publi = nh_.advertise<std_msgs::Float32MultiArray>("detector_out",100);
 
     //sets subscribers
-    image_subs_ = img_tp_.subscribe("image_in", 1, &RosFaceDetectorNode::imageCallback, this);
+    image_subs_ = img_tp_.subscribe("/ros_img_processor/image_out", 1, &RosFaceDetectorNode::imageCallback, this);
     //camera_info_subs_ = nh_.subscribe("camera_info_in", 100, &RosFaceDetectorNode::cameraInfoCallback, this);
 
     //face detector init
-    face_detect_.load("/home/oos/catkin_ws/src/webcam_project/ros_face_detector/filters/lbdcascade_frontlface.xml");
+    face_detect_.load("./src/webcam_project/ros_face_detector/filters/lbdcascade_frontlface.xml");
 }
 
 RosFaceDetectorNode::~RosFaceDetectorNode()
@@ -28,23 +31,28 @@ RosFaceDetectorNode::~RosFaceDetectorNode()
 
 void RosFaceDetectorNode::detect_face()
 {
-
-    image_ = cv_img_ptr_in_->image.clone();
-    cv::cvtColor(image_, gray_, CV_BGR2GRAY);
-    face_detect_.detectMultiScale(image_, faces_, 1.3, 4, cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+    if (cv_img_ptr_in_ != nullptr)
+    {
+      //copy the input image to the out one
+      image_ = cv_img_ptr_in_->image;
+      cv::cvtColor(image_, gray_, CV_BGR2GRAY);
+      face_detect_.detectMultiScale(gray_, faces_, 1.3, 4, cv::CASCADE_SCALE_IMAGE, cv::Size(50, 50), cv::Size(200,200));
+    }
 }
 
 void RosFaceDetectorNode::publish()
 {
-
     detect_msg_.data.clear();
 
-    detect_msg_.data[0] = (float)faces_[0].x;
-    detect_msg_.data[1] = (float)faces_[0].y;
-    detect_msg_.data[2] = (float)faces_[0].width;
-    detect_msg_.data[3] = (float)faces_[0].height;
-
-    detector_publi.publish(detect_msg_);
+    if (faces_.size() > 0)
+    {
+      ROS_INFO("Face detected at (%d,%d) (w=%d,h=%d)", faces_[0].x, faces_[0].y, faces_[0].width, faces_[0].height);
+      detect_msg_.data[0] = (float)faces_[0].x;
+      detect_msg_.data[1] = (float)faces_[0].y;
+      detect_msg_.data[2] = (float)faces_[0].width;
+      detect_msg_.data[3] = (float)faces_[0].height;
+      detector_publi.publish(detect_msg_);
+    }
 }
 
 double RosFaceDetectorNode::getRate() const
@@ -65,5 +73,3 @@ void RosFaceDetectorNode::imageCallback(const sensor_msgs::ImageConstPtr& _msg)
         return;
     }
 }
-
-
