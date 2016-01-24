7f28d7a5e765fce47aa4de28b213dda296df7ce0
Juampe López <jplopez@circutor.com>
2016-01-24 21:50:16 +0100

    Test code.

--- pwm_arduino_v2/pwm_arduino_v2.ino
+++ pwm_arduino_v2/pwm_arduino_v2.ino
@@ -7,22 +7,17 @@ ros::NodeHandle  nh;
 int servoX = 9; //pwm pins
 int servoY = 10;
 
+int xValue;
+int yValue;
+
 void servo_y_cb( const std_msgs::Int32MultiArray& cmd_msg)
-{ 
-  int y;
-  y=cmd_msg.data[0];
- 
-  analogWrite(servoY, y);
-  Serial.println("++++++++++++++++++++++++++++++++++++++");
+{
+  yValue = cmd_msg.data[0];
 }
 
 void servo_cb( const std_msgs::Int32MultiArray& cmd_msg)
-{ 
-  int x;
-  x=cmd_msg.data[0];
- 
-  analogWrite(servoX, x);
-  Serial.println("\n****************************");
+{
+  xValue = cmd_msg.data[0];
 }
 
 ros::Subscriber<std_msgs::Int32MultiArray> sub("/ros_pid_controller/pwm_output_x", servo_cb);
@@ -32,25 +27,19 @@ void setup(){
   pinMode(servoX, OUTPUT); // sets the pin as output
   pinMode(servoY, OUTPUT);
 
-  pinMode(7, OUTPUT);   
+  pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
 
   nh.initNode();
   nh.subscribe(sub);
   nh.subscribe(sub_y);
-
-  Serial.begin(9600);
-  while (!Serial);      
-  Serial.println("\nINIT___________");
-
 }
 
 void loop(){
-  digitalWrite(7,HIGH);   
-  digitalWrite(8,HIGH);    
+  digitalWrite(7,HIGH);
+  digitalWrite(8,HIGH);
   nh.spinOnce();
   delay(10);
-  Serial.println("\n---------------------------------");
+  analogWrite(servoX, xValue);
+  analogWrite(servoY, yValue);
 }
-
-
